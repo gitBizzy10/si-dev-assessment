01a1de6add7eb4b670b0f8d25f7825c39237e6de
gitBizzy <butvin@gmail.com>
2017-04-18 14:01:17 +0000

    Finishing Task 6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,22 @@ class UsersController < ApplicationController
 
 private
 
+
+
   def user_params
     params.require(:user).permit(:id, :name, :email, :password, :password_confirmation)
   end
+
+require 'twilio-ruby'
+
+account_sid = "AC55b1fc93d41fc9a9d5d8e3c5125c3f0e" # Your Account SID from www.twilio.com/console
+auth_token = "{{ 712b4d20dcb4107cda86465794724581 }}"   # Your Auth Token from www.twilio.com/console
+
+@client = Twilio::REST::Client.new account_sid, auth_token
+message = @client.account.messages.create(:body => "Hello from Ruby",
+      :to => "+14324137957",    # Replace with your phone number
+    :from => "+15123573303")  # Replace with your Twilio number
+
+puts message.sid
 
 end
